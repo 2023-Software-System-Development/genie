433fb94fb62122695755579517dc06f763fef1df
gagle1231 <kke1231@naver.com>
2023-05-23 22:45:50 +0900

    [feat]Apply Entity 작성 (#4)

---
+++ src/main/java/com/example/genie/domain/apply/entity/Apply.java
@@ -0,0 +1,42 @@
+package com.example.genie.domain.apply.entity;
+
+import com.example.genie.domain.pot.entity.Pot;
+import com.example.genie.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "apply")
+@Entity
+@DynamicUpdate
+public class Apply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "apply_id")
+    private Long id;
+
+    @Enumerated(EnumType.ORDINAL)
+    private State state;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pot_id")
+    Pot pot;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User applicant;
+
+    @Builder
+    public Apply(Long id, State state, Pot pot, User applicant) {
+        this.id = id;
+        this.state = state;
+        this.pot = pot;
+        this.applicant = applicant;
+    }
+
+}
