a34388307e8e27c2eef3ca2337931a654d89ecd4
seeunjang <wisjang1@naver.com>
2023-05-23 17:17:59 +0900

    [feat] reliability entity 생성(#8)

---
+++ src/main/java/com/example/genie/domain/reliability/entity/Reliability.java
@@ -0,0 +1,27 @@
+package com.example.genie.domain.reliability.entity;
+
+
+import com.example.genie.common.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "reliability")
+@Entity
+@DynamicUpdate
+public class Reliability extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reliability_id")
+    private Long id;
+
+    private String history;
+    private Integer score;
+
+
+}
