259c75ffbf1dff6fe608a6aa6fc34a04d742773a
gagle1231 <kke1231@naver.com>
2023-05-27 17:30:41 +0900

    [feat] 회원가입 기능 구현 (#18)

--- src/main/java/com/example/genie/domain/auth/controller/AuthUserController.java
+++ src/main/java/com/example/genie/domain/auth/controller/AuthUserController.java
@@ -3,10 +3,16 @@ package com.example.genie.domain.auth.controller;
 import com.example.genie.domain.auth.service.AuthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthUserController {
+
     @Autowired
     AuthUserService userAuthUserService;
     @GetMapping("/user/login")
@@ -14,4 +20,21 @@ public class AuthUserController {
         return "login";
     }
 
+    @GetMapping("/user/signup")
+    public String singUpForm(@ModelAttribute UserForm userForm){
+        return "signUp";}
+
+    @PostMapping("/user/signup")
+    public String signUp(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, SessionStatus sessionStatus){
+        if(bindingResult.hasErrors()){
+            return "signup";
+        }
+        userAuthUserService.join(userForm, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "signup";
+        }
+
+        sessionStatus.setComplete();
+        return "redirect:/user/login";
+    }
 }
---
+++ src/main/java/com/example/genie/domain/auth/controller/UserForm.java
@@ -0,0 +1,45 @@
+package com.example.genie.domain.auth.controller;
+
+import com.example.genie.domain.user.entity.User;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+@Data
+public class UserForm {
+    @NotBlank(message = "이름을 입력해주세요")
+    private String userName;
+    @NotBlank(message = "아이디를 입력해주세요")
+    private String userLoginId;
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String userPw;
+    @NotBlank(message = "비밀번호를 확인해주세요")
+    private String userPwCheck;
+    @NotBlank(message = "닉네임을 입력해주세요")
+    private String userNickName;
+    @NotBlank(message = "전화번호를 입력해주세요")
+    private String phoneNumber;
+    @Email
+    private String email;
+
+    @DateTimeFormat
+    private LocalDateTime birth;
+    private String accountNumber;
+    private String bankName;
+
+    public User toEntity(){
+        User user = User.builder()
+                .userName(userName)
+                .userLoginId(userLoginId)
+                .userPw(userPw)
+                .userNickName(userNickName)
+                .phoneNumber(phoneNumber)
+                .email(email)
+                .birth(birth)
+                .accountNumber(accountNumber)
+                .bankName(bankName).build();
+        return user;
+    }
+}
