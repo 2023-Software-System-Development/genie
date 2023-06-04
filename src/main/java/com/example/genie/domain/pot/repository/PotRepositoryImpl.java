48171f7148ba71bd751ca0312e1508e98082f236
seeunjang <wisjang1@naver.com>
2023-06-04 22:58:39 +0900

    [feat] 조건별 검색 기능 추가(#20)

--- src/main/java/com/example/genie/domain/pot/repository/PotRepositoryImpl.java
+++ src/main/java/com/example/genie/domain/pot/repository/PotRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.example.genie.domain.pot.repository;
 
 import com.example.genie.domain.pot.entity.Pot;
 import com.example.genie.domain.pot.entity.QPot;
+import com.example.genie.domain.pot.form.PotSearchForm;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.util.StringUtils.hasText;
 
@@ -19,11 +21,12 @@ public class PotRepositoryImpl implements PotRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Page<Pot> findListBySearch(String searchText, Pageable pageable) {
+    public Page<Pot> findListBySearch(PotSearchForm potSearchForm, Pageable pageable) {
         List<Pot> potList = jpaQueryFactory
                 .select(QPot.pot).distinct()
                 .from(QPot.pot)
-                .where(containWordInPot(searchText))
+                .where(containWordInPot(potSearchForm.getSearchType(), potSearchForm.getSearchText()),
+                        Optional.ofNullable(potSearchForm.getOttType()).map(QPot.pot.ottType::eq).orElse(null))
                 .orderBy(QPot.pot.createdDate.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize() + 1)
@@ -41,9 +44,38 @@ public class PotRepositoryImpl implements PotRepositoryCustom {
         return new PageImpl<>(list, pageable, list.size());
     }
 
-    private BooleanBuilder containWordInPot(String word) {
+    private BooleanBuilder containWordInPot(String searchType, String searchText) {
+        if (searchType != null) {
+            if (searchType.equals("potName"))
+                return containWordInPotName(searchText);
+            else if (searchType.equals("remain"))
+                return containWordInRemain(searchText);
+            else
+                return containWordInTerm(searchText);
+        } else {
+            return new BooleanBuilder();
+        }
+    }
+
+    private BooleanBuilder containWordInPotName(String word) {
         if (hasText(word)) {
-            return new BooleanBuilder(QPot.pot.potName.containsIgnoreCase(word));
+            return new BooleanBuilder(QPot.pot.potName.contains(word));
+        } else {
+            return new BooleanBuilder();
+        }
+    }
+
+    private BooleanBuilder containWordInRemain(String word) {
+        if (hasText(word)) {
+            return new BooleanBuilder(QPot.pot.remain.eq(Integer.valueOf(word)));
+        } else {
+            return new BooleanBuilder();
+        }
+    }
+
+    private BooleanBuilder containWordInTerm(String word) {
+        if (hasText(word)) {
+            return new BooleanBuilder(QPot.pot.term.eq(Integer.valueOf(word)));
         } else {
             return new BooleanBuilder();
         }
