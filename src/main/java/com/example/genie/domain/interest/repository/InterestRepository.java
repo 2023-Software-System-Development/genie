41446ba583ba3884f5a736cd291eda664cadb655
gagle1231 <kke1231@naver.com>
2023-06-17 02:13:57 +0900

    [refeat] 팟 좋아요 기능 구현

--- src/main/java/com/example/genie/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/example/genie/domain/interest/repository/InterestRepository.java
@@ -4,4 +4,5 @@ import com.example.genie.domain.interest.entity.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
+    public Interest findByPot_IdAndUser_Id(Long potId, Long userId);
 }
