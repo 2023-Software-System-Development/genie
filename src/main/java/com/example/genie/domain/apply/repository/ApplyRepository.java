3d6bddf68b0cb35a02e78485bbe56bdef0093b8b
gagle1231 <kke1231@naver.com>
2023-06-20 23:05:19 +0900

    [feat] my pot 보기 기능

--- src/main/java/com/example/genie/domain/apply/repository/ApplyRepository.java
+++ src/main/java/com/example/genie/domain/apply/repository/ApplyRepository.java
@@ -3,7 +3,11 @@ package com.example.genie.domain.apply.repository;
 import com.example.genie.domain.apply.entity.Apply;
 import com.example.genie.domain.apply.entity.State;
 import com.example.genie.domain.pot.entity.Pot;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
@@ -12,7 +16,8 @@ public interface ApplyRepository extends JpaRepository<Apply, Long> {
     public Apply findByPot_IdAndApplicant_Id(Long potId, Long userId);
     public List<Apply> findByStateAndPot_Id(State state, Long potId);
 
-    public List<Apply> findByApplicant_IdAndState(Long userId, State state);
-    public List<Apply> findByPot_MasterId(Long userId);
     public void deleteByStateAndPot_Id(State state, Long potId);
+
+    @Query(value = "select a.pot from Apply a where a.applicant.id = :userId and a.state = :state")
+    public Page<Pot> findPotByUser_Id(Long userId, State state, Pageable pageable);
 }
